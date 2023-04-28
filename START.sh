120567dfc9b67035839ce7df2e7afe8b8798ba8b
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2023-04-28 12:09:47 -0700

    Update START.sh

--- START.sh
+++ START.sh
@@ -74,13 +74,12 @@ function CHOUR(){
     grep -A 1 Daily $CRON_DIR |head -n 2 | tail -n 1 |cut -c 4-5
 }
 #
-#
 echo >> $CRON_DIR
 echo "#Stop tcpdump & check captures" >> $CRON_DIR
 echo $(CMIN)  $(expr $(CHOUR) - 1) "* * 1-6      root    killall tcpdump && sleep 30 && /home/tsec/SCRIPT/checker.sh" >> $CRON_DIR
 #Finished
 toilet -f ivrit '...Instalado'
+sleep 3
 dialog --keep-window --no-ok --no-cancel --backtitle "$myBACKTITLE" --title "[ Reiniciando el sistema... ]" --pause "" 7 80 5
 clear 
-sleep 3
 reboot
