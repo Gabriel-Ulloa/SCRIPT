52b5e28c396d96225a492db22333aa285e22eeee
Gabriel Ulloa <86376043+Gabriel-Ulloa@users.noreply.github.com>
2023-06-21 21:15:57 -0700

    Update START.sh

--- START.sh
+++ START.sh
@@ -88,7 +88,7 @@ function CMIN(){
 }
 #
 function CHOUR(){
-    grep -A 1 Daily $CRON_DIR |head -n 2 | tail -n 1 |cut -c 3-4
+    grep -A 1 Daily $CRON_DIR |head -n 2 | tail -n 1 |cut -c 4-5
 }
 #
 echo >> $CRON_DIR
